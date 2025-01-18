0bebd6ce7d69b5072f8f82752cf6b4de57e30eee
wolfiee42 <saifalislam2022@gmail.com>
2025-01-18 21:51:20 +0600

    cloning the 3rd lectures code to understand react 19 fundamentals

--- thinking in react/thinking-in-react-18/src/features/user-management/components/PageHeader.tsx
+++ thinking in react/thinking-in-react-18/src/features/user-management/components/PageHeader.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export const PageHeader = () => {
   return (
     <div className="flex items-center justify-between">
-      <h1 className="text-2xl font-bold">Heading</h1>
+      <h1 className="text-2xl font-bold">User Management</h1>
       <Button>Add User</Button>
     </div>
   );
--- thinking in react/thinking-in-react-18/src/utils/debounce.ts
+++ thinking in react/thinking-in-react-18/src/utils/debounce.ts
@@ -1,7 +1,4 @@
-export function debounce<T extends (...args: never[]) => void>(
-	func: T,
-	delay: number
-): (...args: Parameters<T>) => void {
+export function debounce<T extends (...args: never[]) => void>(func: T, delay: number): (...args: Parameters<T>) => void {
 	let timeoutId: ReturnType<typeof setTimeout>;
 
 	return (...args: Parameters<T>) => {
