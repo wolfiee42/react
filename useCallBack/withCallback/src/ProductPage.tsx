9fd22d0b2c49d56dde250c84d97f90f986f1137b
wolfiee42 <saifalislam2022@gmail.com>
2025-01-30 08:32:06 +0600

    feat: implementing usecalback in a project to understand how it works

---
+++ useCallBack/withCallback/src/ProductPage.tsx
@@ -0,0 +1,26 @@
+import { useCallback } from "react";
+import ShippingForm from "./ShippingForm";
+
+export default function ProductPage({ productId, referrer, theme }) {
+  const handleSubmit = useCallback(
+    (orderDetails) => {
+      post("/product/" + productId + "/buy", {
+        referrer,
+        orderDetails,
+      });
+    },
+    [productId, referrer]
+  );
+
+  return (
+    <div className={theme}>
+      <ShippingForm onSubmit={handleSubmit} />
+    </div>
+  );
+}
+
+function post(url: string, data: any) {
+  // Imagine this sends a request...
+  console.log("POST /" + url);
+  console.log(data);
+}
